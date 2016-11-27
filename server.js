a7b6af097358487e24f8c3e385477a1c684cf2fc
cmoneverybody <as.avramenko@yandex.ru>
2016-11-27 22:24:22 +0300

    1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var
    app = express(),
    port = process.env.PORT || 777;
 
-app.use(express['static'](__dirname));
+app.use(express.static('app'));
    
 app.listen(port, function () {
     console.log('Listening on port "' + port + '".');
